70b45ced02ac1dff7fe159cb42482c90c9a0b0ab
seanjones2848 <seanjones2848@gmail.com>
2017-09-29 23:39:38 -0700

    changed multiline layout

--- request.js
+++ request.js
@@ -2,8 +2,8 @@ var request = require('request');
 
 module.exports = {
 	ask: function(month) {
-		var request_url = 'https://data.marincounty.org/resource/mw3d-ud6d.json?month_and_year=20' +\
-			(month < 7 ? '16' : '17') + '-' +\
+		var request_url = 'https://data.marincounty.org/resource/mw3d-ud6d.json?month_and_year=20' +
+			(month < 7 ? '16' : '17') + '-' +
 			(month < 10 ? '0' : '') + month + '-01T00:00:00.000'
 		console.log(request_url);
 		request(request_url, function(error, response, body) {
