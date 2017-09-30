51c41878d57911a62e954090c37cd9898895e11b
seanjones2848 <seanjones2848@gmail.com>
2017-09-29 23:28:57 -0700

    request first run

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var charts = require('./charts.js');
+var info = require('./request.js');
 
+info.ask(1);
 console.log("Hello-world");
---
+++ requests.js
@@ -0,0 +1,16 @@
+var request = require('request');
+
+module.export {
+	ask: function(month) {
+		var request_url = 'https://data.marincounty.org/resource/mw3d-ud6d.json?month_and_year=20' +\
+			(month < 7 ? '16' : '17') + '-' +\
+			(month < 10 ? '0' : '') + month + '-01T00:00:00.000'
+		console.log(request_url);
+		request(request_url, function(error, response, body) {
+			console.log('error:', error);
+			console.log('statusCode:', response && response.statusCode);
+			console.log('body:', body);
+			var info = body
+		}
+	}
+}	
