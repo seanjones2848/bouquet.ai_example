a2e6adb8caaf7fe995dfd4a00fe770b967bdf420
seanjones2848 <seanjones2848@gmail.com>
2017-09-30 22:45:58 -0700

    added css, and js to make dropdown

--- index.js
+++ index.js
@@ -16,5 +16,21 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+function dropFunction() {
+	document.getElementById("monthDrop").classlist.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdown = document.getElementsByClassName("dropdown-content");
+		for (var i=0; i < dropdown.length; i++) {
+			var openDropdown = dropdown[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show);
+			}
+		}
+	}
+}
+
 info.ask(1, function(s){console.log(s)});
 console.log("Running just fine");
