2aaf96c865e0227db7616939647cb527f4009216
seanjones2848 <seanjones2848@gmail.com>
2017-09-30 19:11:24 -0700

    added express for html serving

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var charts = require('./charts.js');
 var info = require('./request.js');
+var express = require('express');
+var app = express();
+var path = require('path');
 
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
 info.ask(1, function(s){console.log(s)});
 console.log("Running just fine");
