5a13fba643de2214324727fe4486018c0532ec76
seanjones2848 <seanjones2848@gmail.com>
2017-09-30 20:25:43 -0700

    making better index.js

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname + '/index.html'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 info.ask(1, function(s){console.log(s)});
 console.log("Running just fine");
