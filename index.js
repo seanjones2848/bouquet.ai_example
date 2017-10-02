66b3025acc062000d3e9aee113b8196854e149b1
seanjones2848 <seanjones2848@gmail.com>
2017-10-01 21:55:03 -0700

    rearranging index.js to get window to work from new computer

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdown = document.getElementsByClassName("dropdown-content");
+		for (var i=0; i < dropdown.length; i++) {
+			var openDropdown = dropdown[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/'));
@@ -20,17 +32,7 @@ function dropFunction() {
 	document.getElementById("monthDrop").classlist.toggle("show");
 }
 
-window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-		var dropdown = document.getElementsByClassName("dropdown-content");
-		for (var i=0; i < dropdown.length; i++) {
-			var openDropdown = dropdown[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
+
 
 info.ask(1, function(s){console.log(s)});
 console.log("Running just fine");
